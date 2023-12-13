89fc1046b063355a365f3783d8cc67ab4e5c77f9
Anushka.Panagar <anushka.panagar@ibm.com>
2023-12-13 10:39:21 -0700

    Added AuthGuard for restricting the profile route without authentication

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { UserProfile } from '../components/user-profile-details/user-profile-details.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  currentUser$ = new BehaviorSubject<{ name: string } | null | undefined>(
+    undefined
+  );
   constructor(private apiService: ApiService) {}
+
+  setCurrentUser() {
+    if (localStorage.getItem('token')) {
+      // get user and set the current user
+      this.currentUser$.next(null);
+    } else {
+      this.currentUser$.next({ name: 'King Julien' });
+    }
+  }
 
   public async registerUserForm() {
     const path = 'https://mocki.io/v1/7f434df6-a4ac-4817-ab7c-dd39a564d01d';
